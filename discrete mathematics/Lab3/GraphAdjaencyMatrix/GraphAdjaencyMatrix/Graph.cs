76fdd7fab0314b79838b8d37b5f92d97295cc9ef
Daniel <shernoknignik@gmail.com>
2025-02-20 21:20:13 +0300

    DescretMath Lab3 ConnectiveMatrix addada

---
+++ discrete mathematics/Lab3/GraphAdjaencyMatrix/GraphAdjaencyMatrix/Graph.cs
@@ -0,0 +1,346 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphConnectivityMatrix
+{
+    internal class Graph
+    {
+        static int[,] adjaencyMatrix;
+        static string points = "abcdefghij";
+        static int currentNumber = 1;
+
+        private static int[,] GetSavedMatrix(int number)
+        {
+            string filePath = $@"..\..\Matrices\matrix{number}.txt";
+
+            // Считываем данные из файла и определяем размер матрицы
+            int[,] matrix;
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string[] lines = File.ReadAllLines(filePath);
+                int rows = lines.Length;
+                int cols = lines[0].Split(' ').Length;
+
+                matrix = new int[rows, cols];
+
+                // Заполняем матрицу данными из файла
+                for (int i = 0; i < rows; i++)
+                {
+                    string[] values = lines[i].Split(' ');
+                    for (int j = 0; j < cols; j++)
+                    {
+                        matrix[i, j] = int.Parse(values[j]);
+                    }
+                }
+                reader.Close();
+                return matrix;
+            }
+        }
+
+        private static int[,] Sum(int[,] m1, int[,] m2)
+        {
+            for (int i = 0; i < m1.GetLength(1); i++)
+            {
+                for (int j = 0; j < m1.GetLength(1); j++)
+                {
+                    if (m1[i, j] + m2[i, j] > 0)
+                        m1[i, j] = 1;
+                }
+            }
+            return m1;
+        }
+
+        private static int[,] Transpose(int[,] m)
+        {
+            int[,] transponseM = (int[,])m.Clone();
+
+            for (int i = 0, it = 0; i < transponseM.GetLength(1); i++, it++)
+            {
+                for (int j = 0, jt = 0; j < transponseM.GetLength(1); j++, jt++)
+                {
+                    transponseM[jt, it] = m[i, j];
+                }
+            }
+            return transponseM;
+        }
+
+        private static void LogicAnd(ref int[,] m1, int[,] m2)
+        {
+            for (int i = 0; i < m1.GetLength(1); i++)
+            {
+                for (int j = 0; j < m1.GetLength(1); j++)
+                {
+                    m1[i, j] = m1[i, j] & m2[i, j];
+                }
+            }
+        }
+
+        private static int[,] MultiplyMatrices(int[,] m1, int[,] m2)
+        {
+            int[,] result = (int[,])m1.Clone();
+            int n = m1.GetLength(1);
+            for (int i = 0/*, i1 = 0, i2 = 0*/; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    for (int k = 0; k < n; k++)
+                    {
+                        if (result[i, j] + m1[i, k] * m2[k, j] > 0)
+                            result[i, j] = 1;
+                    }
+                }
+            }
+            return result;
+        }
+
+        private static int[,] PowMatrix(int[,] m, int degree)
+        {
+            int[,] result = (int[,])m.Clone();
+            for (int i = 0; i < degree; i++)
+            {
+                result = MultiplyMatrices(result, m);
+            }
+            return result;
+        }
+
+        private static int[,] GetReachabilityMatrix()
+        {
+            int n = adjaencyMatrix.GetLength(1);
+            int[,] result = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == j)
+                        result[i, j] = 1;
+                }
+            }
+
+            for (int i = 0; i < adjaencyMatrix.GetLength(1); i++)
+            {
+                result = Sum(result, PowMatrix(adjaencyMatrix, i));
+            }
+            return result;
+        }
+
+        private static int[,] GetConnectivityMatrix(/*List<List<int>> r*/)
+        {
+            int[,] r = GetReachabilityMatrix();
+            LogicAnd(ref r, Transpose(r));
+            return r;
+        }
+
+        private static void DeleteRowCol(int number, int[,] m/*int row, int col*/)
+        {
+            int n = m.GetLength(1);
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == number || j == number)
+                        m[i, j] = 0;
+                }
+            }
+        }
+
+        private static List<int> GetComponent(int[,] connectivityMatrix, int row)
+        {
+            List<int> result = new List<int>();
+            for (int j = 0; j < connectivityMatrix.GetLength(1); j++)
+            {
+                if (connectivityMatrix[row, j] > 0)
+                {
+                    result.Add(j);
+                }
+            }
+            for (int i = 0; i < result.Count; i++)
+                DeleteRowCol(result[i], connectivityMatrix);
+            return result;
+        }
+
+        private static List<List<int>> GetConnectivityComponents()
+        {
+            List<List<int>> result = new List<List<int>>();
+            int[,] connectivityMatrix = GetConnectivityMatrix();
+
+            for (int i = 0; i < adjaencyMatrix.GetLength(1); i++)
+            {
+                List<int> k = GetComponent(connectivityMatrix, i);
+                if (k.Count > 0)
+                {
+                    result.Add(k);
+                }
+            }
+            return result;
+        }
+
+        public static void ChooseMatrix(int number)
+        {
+            string[] lines = File.ReadAllLines(@"..\..\Matrices\matrixNumber.txt");
+            int numberMax = int.Parse(lines[0]);
+            if (number > numberMax)
+            {
+                Console.WriteLine("Введены не корректные данные, повторите");
+                return;
+            }
+
+            currentNumber = number;
+            adjaencyMatrix = GetSavedMatrix(number);
+            Console.WriteLine("Загруженная матрица:");
+            for (int i = 0; i < adjaencyMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < adjaencyMatrix.GetLength(1); j++)
+                {
+                    Console.Write(adjaencyMatrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void ShowConnectivityComponents()
+        {
+            if(adjaencyMatrix == null)
+            {
+                Console.WriteLine("Еще не выбрана матрица смежности");
+                return;
+            }    
+            List<List<int>> connectivityComponents = GetConnectivityComponents();
+
+            for (int i = 0; i < connectivityComponents.Count; i++)
+            {
+                string component = $"K{i + 1} : ";
+                for (int j = 0; j < connectivityComponents[i].Count; j++)
+                {
+                    component += points[connectivityComponents[i][j]];
+                }
+                Console.WriteLine(component);
+            }
+        }
+
+        public static void DisplayAllSavedMatrix()
+        {
+            string filePath = @"..\..\Matrices\matrixNumber.txt";
+            int number;
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                // Считываем строки из файла
+                string[] lines = File.ReadAllLines(filePath);
+                number = int.Parse(lines[0]);
+                reader.Close();
+            }
+            for (int k = 1; k <= number; k++)
+            {
+                int[,] matrix = GetSavedMatrix(k);
+                Console.WriteLine($"Матрица смежности {k}:");
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                    {
+                        Console.Write(matrix[i, j] + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        public static void InputMatrix(int size) 
+        {
+            if (size < 0 || size > 10)
+            {
+                Console.WriteLine();
+                return;
+            }
+
+            int[,] matrix = new int[size, size];
+
+            Console.WriteLine("Введите элементы матрицы:");
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    External_Interactions.Input(ref matrix[i, j], 0, 1, $"Введите элемент [{i + 1}][{j + 1}]: ");
+                }
+            }
+
+            Console.WriteLine("Введенная матрица:");
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            adjaencyMatrix = matrix;
+            currentNumber = 0;
+        }
+
+        public static void SaveMatrix()
+        {
+            if (adjaencyMatrix == null)
+            {
+                Console.WriteLine("Еще не выбрана ни одна матрица смежности");
+                return;
+            }
+            if (currentNumber == 0)
+            {
+                string path = @"..\..\Matrices\matrixNumber.txt";
+                int number;
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    string[] lines = File.ReadAllLines(path);
+                    number = int.Parse(lines[0]);
+                    reader.Close();
+                }
+                number++;
+                File.WriteAllText(path, number.ToString());
+                currentNumber = number;
+            }
+
+
+            //int[,] matrix = {
+            //    {0, 1, 0 },
+            //    { 0, 0, 1},
+            //    { 1, 0, 0}
+            ////{ 0, 0, 0, 1 ,0, 1, 0},
+            ////{ 0, 0, 0, 0, 1, 0, 1},
+            ////{ 0, 1, 0, 0, 0, 1, 0},
+            ////{ 1, 0, 1, 0, 1, 0, 0},
+            ////{ 0, 0, 0, 0, 0, 0, 1},
+            ////{ 0, 0, 1, 0, 0, 1, 1},
+            ////{ 0, 0, 0, 0, 1, 0, 0}
+            //};
+
+            int[,] matrix = adjaencyMatrix;
+            string filePath = $@"..\..\Matrices\matrix{currentNumber}.txt";
+
+            // Записываем матрицу в файл
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                int rows = matrix.GetLength(0); // Количество строк
+                int cols = matrix.GetLength(1); // Количество столбцов
+
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < cols; j++)
+                    {
+                        writer.Write(matrix[i, j]);
+
+                        if (j < cols - 1)
+                        {
+                            writer.Write(" ");
+                        }
+                    }
+                    writer.WriteLine();
+                }
+            }
+
+            Console.WriteLine("Матрица успешно записана в файл.");
+        }
+
+    } 
+}
---
+++ discrete mathematics/Lab3/GraphAdjaencyMatrix/GraphAdjaencyMatrix/Programm.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphConnectivityMatrix
+{
+    internal class Programm
+    {
+        public static void Main() 
+        {
+            int command = 0;
+            while (command != 4)
+            {
+                Console.WriteLine("1. Задать матрицу смежности\n2. Вывести компоненты связности\n3. Сохранить введенную матрицу смежности\n4. Выход");
+                External_Interactions.Input(ref command, 1, 4);
+                switch (command) 
+                {
+                    case 1:
+                        {
+                            while (command != 3)
+                            {
+                                Console.WriteLine("1. Выбрав из имеющихся вариантов\n2. Введя с клавиатуры\n3. Назад");
+                                External_Interactions.Input(ref command, 1, 3);
+                                switch (command)
+                                {
+                                    case 1:
+                                        {
+                                            Graph.DisplayAllSavedMatrix();
+                                            External_Interactions.Input(ref command, 1, 15, "Введите номер матрицы, которую хотите загрузить");
+                                            Graph.ChooseMatrix(command);
+                                            command = 3;
+                                            break;
+                                        }
+                                    case 2:
+                                        {
+                                            External_Interactions.Input(ref command, 2, 10, "Введите размер матрицы, которую хотите задать");
+                                            Graph.InputMatrix(command);
+                                            command = 3;
+                                            break;
+                                        }
+                                    case 3:
+                                        {
+                                            break;
+                                        }
+                                    default:                                        
+                                        break;
+                                }
+                            }
+                            //command = 0;
+                            break;
+                        }
+                    case 2: 
+                        {
+                            Graph.ShowConnectivityComponents();
+                            break;
+                        }
+                    case 3:
+                        {
+                            Graph.SaveMatrix();
+                            break;
+                        }
+                    case 4: 
+                        {
+                            break;
+                        }
+                }
+                
+                
+            }
+        }
+
+        public static void OldMain()
+        {
+            Graph.DisplayAllSavedMatrix();
+            Graph.ChooseMatrix(1);
+            Graph.ShowConnectivityComponents();
+            //Graph.ShowConnectivityComponents();
+        }
+    }
+}
